be91710db94751d9ff78509186d414f0ab774ca6
Youssefragab26 <youssef26ragab@gmail.com>
2024-09-13 20:58:45 +0300

    update

--- Portfolio/Education.js
+++ Portfolio/Education.js
@@ -1,8 +1,29 @@
-//body style
-document.body.style.backgroundColor = "#155263";
-document.body.style.color = "#fff"
 
+//Styling...
+//Body Style
+document.body.style.backgroundColor = "#144935";
 
-//body
-let cn = document.getElementById("cn");
-cn.style.backgroundColor = "#d55800";
+//Back Button Style
+let BackButton = document.getElementById("back")
+BackButton.style.backgroundColor = "#d55800"
+BackButton.style.color = "#18191b"
+BackButton.style.border = "1px solid #ff9b53"
+BackButton.style.borderRadius = "9px"
+BackButton.style.width = "80px"
+BackButton.style.height = "30px"
+BackButton.style.fontSize = "14px"
+BackButton.style.cursor = "pointer"
+
+//Head Style
+let CollegeName = document.getElementById("h1");
+let BachelorName = document.getElementById("h2");
+let DepartmentName = document.getElementById("h3");
+CollegeName.style.textAlign = "center";
+BachelorName.style.textAlign = "center";
+DepartmentName.style.textAlign = "center";
+CollegeName.style.fontFamily = "Arial";
+BachelorName.style.fontFamily = "Arial";
+DepartmentName.style.fontFamily = "Arial";
+CollegeName.style.color = "#cba841";
+BachelorName.style.color = "#cba841";
+DepartmentName.style.color = "#cba841";
