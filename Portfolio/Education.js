d7cc97a99db215ade5d1babb47146f2dde5ae813
Youssefragab26 <youssef26ragab@gmail.com>
2024-09-14 22:57:45 +0300

    update

--- Portfolio/Education.js
+++ Portfolio/Education.js
@@ -1,29 +1,31 @@
+//Elements..
+let BackButton = document.getElementById("back");
+let CollegeName = document.getElementById("h1");
+let BachelorName = document.getElementById("h2");
+let DepartmentName = document.getElementById("h3");
+
 
 //Styling...
 //Body Style
 document.body.style.backgroundColor = "#144935";
+document.body.style.fontFamily = "Arial";
 
 //Back Button Style
-let BackButton = document.getElementById("back")
-BackButton.style.backgroundColor = "#d55800"
-BackButton.style.color = "#18191b"
-BackButton.style.border = "1px solid #ff9b53"
-BackButton.style.borderRadius = "9px"
-BackButton.style.width = "80px"
-BackButton.style.height = "30px"
-BackButton.style.fontSize = "14px"
-BackButton.style.cursor = "pointer"
+BackButton.style.backgroundColor = "#cba841";
+BackButton.style.color = "#144935";
+BackButton.style.border = "1px solid #144935";
+BackButton.style.borderRadius = "9px";
+BackButton.style.width = "80px";
+BackButton.style.height = "30px";
+BackButton.style.fontSize = "14px";
+BackButton.style.cursor = "pointer";
 
 //Head Style
-let CollegeName = document.getElementById("h1");
-let BachelorName = document.getElementById("h2");
-let DepartmentName = document.getElementById("h3");
 CollegeName.style.textAlign = "center";
-BachelorName.style.textAlign = "center";
-DepartmentName.style.textAlign = "center";
-CollegeName.style.fontFamily = "Arial";
-BachelorName.style.fontFamily = "Arial";
-DepartmentName.style.fontFamily = "Arial";
 CollegeName.style.color = "#cba841";
+
+BachelorName.style.textAlign = "center";
 BachelorName.style.color = "#cba841";
+
+DepartmentName.style.textAlign = "center";
 DepartmentName.style.color = "#cba841";
