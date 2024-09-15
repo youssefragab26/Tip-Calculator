c574173c613efbfd3c8a519b504dcb12eb2a4ddc
Youssefragab26 <youssef26ragab@gmail.com>
2024-09-15 14:50:12 +0300

    update

--- Portfolio/Education.js
+++ Portfolio/Education.js
@@ -3,12 +3,13 @@ let BackButton = document.getElementById("back");
 let CollegeName = document.getElementById("h1");
 let BachelorName = document.getElementById("h2");
 let DepartmentName = document.getElementById("h3");
-
+let div1 = document.getElementById("cn");
 
 //Styling...
 //Body Style
-document.body.style.backgroundColor = "#144935";
+document.body.style.backgroundColor = "#103a2a";
 document.body.style.fontFamily = "Arial";
+document.body.style.display = "grid";
 
 //Back Button Style
 BackButton.style.backgroundColor = "#cba841";
@@ -29,3 +30,10 @@ BachelorName.style.color = "#cba841";
 
 DepartmentName.style.textAlign = "center";
 DepartmentName.style.color = "#cba841";
+
+div1.style.backgroundColor = "#144935"
+div1.style.border = "1px solid #144935"
+div1.style.borderRadius = "18px";
+div1.style.marginTop = '0px';
+div1.style.marginLeft = "300px"
+div1.style.marginRight = "300px"
